9c20c484dd1ac6875920e88745c96b57d4894f26
Artur Fiedorowicz <afiedorowicz@brightsolid.com>
2014-10-24 16:43:22 +0100

    Some changes

---
+++ Entity-Framework-In-The-Enterprise/BreakAway.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BreakAway.Web.MvcApplication" Language="C#" %>
--- Entity-Framework-In-The-Enterprise/Model/Trip.cs
+++ Entity-Framework-In-The-Enterprise/Model/Trip.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Model
 {
