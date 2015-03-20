0f9098285331d99a4f95afed1815c78b9f239ab4
afiedorowicz <afiedorowicz@dctfh.com>
2015-03-20 15:08:53 +0000

    New course: Implementing an API in ASP.NET Web API

---
+++ Implementing an API in ASP.NET Web API/countingks-master/CountingKs/Controllers/FoodsController.cs
@@ -0,0 +1,40 @@
+﻿using CountingKs.Data;
+using CountingKs.Data.Entities;
+using CountingKs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CountingKs.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        ICountingKsRepository _repo;
+        ModelFactory _modelFactory;
+
+        public FoodsController(ICountingKsRepository repo)
+        {
+            _repo = repo;
+            _modelFactory = new ModelFactory();
+        }
+
+        public IEnumerable<FoodModel> Get()
+        {
+            var results = _repo.GetAllFoodsWithMeasures()
+                              .OrderBy(f => f.Description)
+                              .Take(25)
+                              .ToList()
+                              .Select(f => _modelFactory.Create(f));
+
+            return results;
+        }
+
+        public FoodModel Get(int id)
+        {
+            return _modelFactory.Create(_repo.GetFood(id));
+        }
+    }
+}
